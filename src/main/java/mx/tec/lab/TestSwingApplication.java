4b1c3e0e085414b471694f7d7ab85c29bcc4f10a
JesusSierra <jsierraher@gmail.com>
2021-05-07 11:08:35 -0600

    first commit

---
+++ src/main/java/mx/tec/lab/TestSwingApplication.java
@@ -0,0 +1,13 @@
+package mx.tec.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSwingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSwingApplication.class, args);
+	}
+
+}
